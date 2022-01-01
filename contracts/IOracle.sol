ba9154f395630d5713d6e447542f49ec522498ab
gurunbox-contracts <otokikamoi@yahoo.co.jp>
2022-01-01 13:33:59 +0900

    event TransferTrustee is not used function have to return address array according the trustee array.

--- contracts/IOracle.sol
+++ contracts/IOracle.sol
@@ -4,12 +4,11 @@ pragma solidity ^0.8.0;
 
 interface IOracle {
     event Judged(address trustee, bool TF);
-    event TransferTrustee(address indexed previousTrustee, address indexed newTrustee, uint trusteeId);
 
     function condition() external view returns (bool);
     function name() external view returns (string memory);
     function conditionCounter() external view returns (uint);
-    function trustees(uint) external view returns (address);
+    function trustees() external view returns (address[] memory);
     function numerator() external view returns (uint);
     function denominator() external view returns (uint);
 
