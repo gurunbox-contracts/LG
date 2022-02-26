eaa2a1035030b767510e92cc0a8dd18461e885d7
gurunbox-contracts <otokikamoi@yahoo.co.jp>
2022-02-26 23:53:58 +0900

    save for getTrusteeIds

--- test/Oracle.test.ts
+++ test/Oracle.test.ts
@@ -16,7 +16,8 @@ describe("Oracle deployed and set 3 of 5", function() {
     let trustees: string[];
 
     beforeEach(async function() {
-        [owner, alice, trustee0, trustee1, trustee2, trustee3, trustee4] = await ethers.getSigners();
+        [owner, alice, trustee0, trustee1, trustee2, trustee3] = await ethers.getSigners();
+        trustee4 = trustee2;
         trustees = [
             trustee0.address,
             trustee1.address,
@@ -36,16 +37,20 @@ describe("Oracle deployed and set 3 of 5", function() {
         expect(await oracle.owner()).to.equal(owner.address);
     });
 
-    it("return 5 trustees with 3 numerator and false condition when initialized", async function() {
+    it("return 6 trustees, 3 numerator and false condition when initialized", async function() {
         expect(await oracle.trustees(0)).to.equal(trustee0.address);
         expect(await oracle.trustees(1)).to.equal(trustee1.address);
         expect(await oracle.trustees(2)).to.equal(trustee2.address);
         expect(await oracle.trustees(3)).to.equal(trustee3.address);
-        expect(await oracle.trustees(4)).to.equal(trustee4.address);
+        expect(await oracle.trustees(4)).to.equal(trustee2.address);
 
         expect(await oracle.numerator()).to.equal(3);
         expect(await oracle.denominator()).to.equal(5);
         expect(await oracle.conditionCounter()).to.equal(0);
         expect(await oracle.condition()).to.equal(false);
+    })
+
+    it("return trusteeIds of each trustee address", async function() {
+        // expect(await oracle.getTrusteeIds(trustee0.address)).to.equal(0);
     })
 })
