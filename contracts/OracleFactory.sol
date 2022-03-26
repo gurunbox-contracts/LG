5d8f7d360be88a12016d2c8e895ac984ea292280
gurunbox-contracts <otokikamoi@yahoo.co.jp>
2022-03-27 00:03:24 +0900

    save OracleFactory.sol . done to write draft NTT

--- contracts/OracleFactory.sol
+++ contracts/OracleFactory.sol
@@ -2,14 +2,38 @@
 
 pragma solidity ^0.8.0;
 
+import { ERC721 } from './@OpenZeppelin/contracts/token/ERC721/ERC721.sol';
 import { IOracleFactory } from './interfaces/IOracleFactory.sol';
 import { IOracle } from './interfaces/IOracle.sol'; 
 import { Oracle } from './Oracle.sol';
+import { Counters } from './utils/Counters.sol';
 import { Create2 } from './@OpenZeppelin/contracts/utils/Create2.sol';
 
-contract OracleFactory is IOracleFactory {
+contract OracleFactory is ERC721, IOracleFactory {
+    using Counters for Counters.Counter;
+
+    Counters.Counter private _tokenIdTracker;
+
     uint256 private nextOracleId;
+    bool private constant transferable = false; 
+
+    // Mapping from oracleId to Oracle address
     mapping(uint256 => address) public override getOracles;
+
+    // Mapping from token ID to owner address
+    mapping(uint256 => address) private _owners;
+
+    // Mapping owner address to token count
+    mapping(address => uint256) private _balances;
+
+    // Mapping from token ID to approved address
+    mapping(uint256 => address) private _tokenApprovals;
+
+    // Mapping from owner to operator approvals
+    mapping(address => mapping(address => bool)) private _operatorApprovals;
+
+    // Alert NFT should be minted to owner when condition is met.
+    constructor() ERC721("Alert NFT", "ALERT") { }
 
     function createOracle(
         string memory name_, 
@@ -28,5 +52,86 @@ contract OracleFactory is IOracleFactory {
         nextOracleId++;
 
         emit OracleCreated(oracle, nextOracleId - 1, name_, _owner);
+    }
+
+    function mint(address to, uint256 oracleId) public {
+        require(msg.sender == getOracles[oracleId], "OracleFactory: caller is not oracle");
+
+        // We cannot just use balanceOf to create the new tokenId because tokens
+        // can be burned (destroyed), so we need a separate counter.
+        _mint(to, _tokenIdTracker.current());
+        _tokenIdTracker.increment();
+    }
+
+    function burn(uint256 tokenId, uint256 oracleId) public virtual {
+        require(msg.sender == getOracles[oracleId], "OracleFactory: caller is not oracle");
+
+        _burn(tokenId);
+    }
+
+    /**
+     * @dev Transfers `tokenId` from `from` to `to`.
+     *  As opposed to {transferFrom}, this imposes no restrictions on msg.sender.
+     *
+     * Requirements:
+     *
+     * - `to` cannot be the zero address.
+     * - `tokenId` token must be owned by `from`.
+     *
+     * Emits a {Transfer} event.
+     */
+    function _transfer(
+        address from,
+        address to,
+        uint256 tokenId
+    ) internal override {
+        require(transferable, "NTT");
+        require(ERC721.ownerOf(tokenId) == from, "ERC721: transfer from incorrect owner");
+        require(to != address(0), "ERC721: transfer to the zero address");
+
+        _beforeTokenTransfer(from, to, tokenId);
+
+        // Clear approvals from the previous owner
+        _approve(address(0), tokenId);
+
+        _balances[from] -= 1;
+        _balances[to] += 1;
+        _owners[tokenId] = to;
+
+        emit Transfer(from, to, tokenId);
+
+        _afterTokenTransfer(from, to, tokenId);
+    }
+
+    /**
+     * @dev See {IERC721-approve}.
+     */
+    function approve(address to, uint256 tokenId) public override {
+        require(transferable, "NTT");
+        address owner = ERC721.ownerOf(tokenId);
+        require(to != owner, "ERC721: approval to current owner");
+
+        require(
+            _msgSender() == owner || isApprovedForAll(owner, _msgSender()),
+            "ERC721: approve caller is not owner nor approved for all"
+        );
+
+        _approve(to, tokenId);
+    }
+
+    /**
+     * @dev Approve `operator` to operate on all of `owner` tokens
+     *
+     * Emits a {ApprovalForAll} event.
+     */
+    function _setApprovalForAll(
+        address owner,
+        address operator,
+        bool approved
+    ) internal override {
+        require(transferable, "NTT");
+        require(owner != operator, "ERC721: approve to caller");
+        _operatorApprovals[owner][operator] = approved;
+        emit ApprovalForAll(owner, operator, approved);
     }
 }
--- contracts/interfaces/IOracleFactory.sol
+++ contracts/interfaces/IOracleFactory.sol
@@ -2,8 +2,11 @@
 
 pragma solidity ^0.8.0;
 
+import { IERC721 } from './IERC721.sol';
+
 interface IOracleFactory {
     event OracleCreated(address indexed oracle, uint256 oracleId, string name, address owner);
+
     function getOracles(uint256 oracleId) external view returns (address);
     function createOracle(
         string memory name_, 
